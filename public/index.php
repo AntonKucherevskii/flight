98350ab087f29b6db6066292d66222fab9aee205
designer <oboefm@gmail.com>
2020-01-27 15:46:33 +0300

    Update 1

--- public/index.php
+++ public/index.php
@@ -1,15 +1,35 @@
 <?php
+use vendor\core\Router;
 
 $query = $_SERVER['QUERY_STRING'];
+
+define('WWW', __DIR__);
+define('CORE', dirname(__DIR__).'/vendor/core');
+define('ROOT', dirname(__DIR__));
+define('APP', dirname(__DIR__).'/app');
+define('LAYOUT', 'default');
+
 require_once(__DIR__ . '/../vendor/libs/function.php');
-require_once(__DIR__ . '/../vendor/core/Router.php');
+
+// Автозагрузка классов
+spl_autoload_register(function($class){
+    $class = str_replace('\\', '/', $class);
+    $file = ROOT.'/'.$class.'.php';
+    if(is_file($file)){
+        require_once $file;
+    }
+});
+
+//Добавление маршрутов
+Router::add('^page/(?P<action>[a-z-]+)/(?P<alias>[a-z-]+)$', ['controller' => 'Page']);
+Router::add('^page/(?P<alias>[a-z-]+)$', ['controller' => 'Page', 'action' => 'view']);
+
+//Defaults routs
+Router::add('^$', ['controller' => 'main', 'action' => 'index']);
+Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
+//--------------------
+
+Router::dispatch($query);
 
 
-Router::add('', ['controller' => 'main', 'action' => 'index']);
 
-
-if (Router::matchRoute($query)) {
-    debug(Router::getRoute());
-} else {
-    print '404';
-}
--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -1,17 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Антон
- * Date: 18.01.2020
- * Time: 13:46
- */
+namespace vendor\core;
 
 class Router
 {
     protected static $routes = [];
     protected static $route = [];
 
-    public static function add($url, $route)
+
+    /**
+     * Добавляет маршруты
+     * @param $url Регулярное выражение соответствующее запросу
+     * @param array $route Маршрут (controller, action)
+     */
+    public static function add($url, $route = [])
     {
         self::$routes[$url] = $route;
     }
@@ -21,24 +22,109 @@ class Router
         return self::$routes;
     }
 
+
+    /**
+     * Задает текущий маршрут
+     * @return array массив с (controller, action)
+     */
     public static function getRoute()
     {
         return self::$route;
     }
 
+
+    /**
+     * Вычисляет текущий маршрут (controller, action) исходя из запроса
+     * @param $query
+     * @return bool
+     */
     public static function matchRoute($query)
     {
-        foreach (self::$routes as $k => $r)
-        {
-            if($query==$k)
-            {
+
+        foreach (self::$routes as $k => $r) {
+            if (preg_match("#$k#i", $query, $matches)) {
+
+                foreach($matches as $m => $v)
+                {
+                    if (is_string($m)) {
+                        $r[$m] = $v;
+                    }
+                }
+                if(!isset($r['action'])) {
+                    $r['action'] = 'index';
+                }
                 self::$route = $r;
+                self::$route['controller'] = self::upperCamelCase(self::$route['controller']);
                 return true;
             }
+        }
+        return false;
+    }
 
-            return false;
+    /**
+     * перенаправляет по корректному маршруту
+     * @param string $query входящая строка запроса
+     * return void
+     */
+    public static function dispatch($query)
+    {
+        $query = self::removeQueryString($query);
+        if (Router::matchRoute($query)) {
+            $controller = 'app\\controller\\' . self::upperCamelCase(self::$route['controller']) . 'Controller';
+
+            if(class_exists($controller)){
+                $cObj = new $controller(self::$route);
+                $action = self::lowerCamelCase(self::$route['action']).'Action';
+                if(method_exists($cObj, $action)){
+                    $cObj->$action();
+                    $cObj->getView();
+                }else{
+                    echo 'Метод: '.$action.' отсутствует';
+                }
+            }else{
+                echo 'Контроллер: '.$controller.' отсутствует';
+            }
+
+        } else {
+            http_response_code(404);
+            include_once '404.html';
         }
     }
 
+    /**
+     * Приводит имена к виду 'NameController'
+     * @param $name
+     * @return mixed|string
+     */
+    protected static function upperCamelCase($name)
+    {
+        $name = str_replace('-', ' ', $name);
+        $name = ucwords($name);
+        $name = str_replace(' ', '', $name);
+        return $name;
+    }
 
+    /**
+     * Приводит имена к виду 'nameAction'
+     * @param $name
+     * @return mixed|string
+     */
+    protected static function lowerCamelCase($name)
+    {
+        $name = self::upperCamelCase($name);
+        $name = lcfirst($name);
+        return $name;
+    }
+
+    protected static function removeQueryString($url)
+    {
+        $params = explode('&', $url, 2);
+        if(false===strpos($params[0], '='))
+        {
+            return rtrim($params[0], '/');
+        }else{
+            return '';
+        }
+        return $url;
+    }
 }
